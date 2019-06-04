9e6baddf2042b522809f08d0eb3fcbd042c9354d
Apoorv Jsohi <apoorv@binary.com>
2019-06-04 16:36:59 +0800

    remove logentries

--- cookbooks/logentries_rsyslog_ng/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'logentries_rsyslog_ng'
-maintainer       'Kostiantyn Lysenko'
-maintainer_email 'gshaud@gmail.com'
-license          'apache2'
-description      'Installs/Configures logentries_rsyslog_ng'
-long_description 'Installs/Configures logentries_rsyslog_ng'
-version          '0.0.7'
-
-supports 'debian'
